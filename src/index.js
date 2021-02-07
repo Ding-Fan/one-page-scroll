11ad762e0a25d7b237549f2cf8facf81c940f6d8
Fan Ding <zichuange@gmail.com>
2021-02-07 20:35:17 +0800

    Version 1.0.0
    Add all Shots
    
    Change data
    
    Modify 'build' command

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import "~/src/scss/index.scss";
-import App from "~/src/components/app/App";
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { resetContext, getContext } from "kea";
-import { Provider } from "react-redux";
+import '~/src/scss/index.scss'
+import App from '~/src/components/app/App'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { resetContext, getContext } from 'kea'
+import { Provider } from 'react-redux'
 
 resetContext({
   // 👈 add this
@@ -14,16 +14,40 @@ resetContext({
   plugins: [
     // additional kea plugins
   ],
-});
+})
 
-const mountNode = document.getElementById("app");
+const mountNode = document.getElementById('app')
+
+let appComponent = <App />
+
+console.log(
+  '🚀 ~ file: index.js ~ line 24 ~ window.mobileCheck()',
+  window.mobileCheck()
+)
+if (window.mobileCheck()) {
+  appComponent = (
+    <div
+      style={{
+        fontSize: '3rem',
+        fontFamily: 'sans-serif',
+        textAlign: 'center',
+        top: '50vh',
+        position: 'relative',
+        transform: 'translateY(-50%)',
+        padding: '3rem',
+        color: '#e87a90',
+        lineHeight: 1.5,
+      }}
+    >
+      请使用电脑端打开本页面以提升展示效果
+    </div>
+  )
+}
 
 const mainContent = (
   <Provider store={getContext().store}>
-    <Router>
-      <App />
-    </Router>
+    <Router>{appComponent}</Router>
   </Provider>
-);
+)
 
-ReactDOM.render(mainContent, mountNode);
+ReactDOM.render(mainContent, mountNode)
